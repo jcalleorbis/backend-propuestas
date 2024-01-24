b4cecf1b9d9cf6260288f3d0a1863653c9e0df86
Jorge Luis Calle Morales <jcalle@unifiedsoftware.us>
2024-01-23 20:23:06 -0500

    Complete the remove cliente

---
+++ functions/eliminarCliente.js
@@ -0,0 +1,59 @@
+exports = async function (request, response) {
+    try {
+      await context.functions.execute(
+        "middlewareVerificarOrigenClientId",
+        request,
+        response
+      );
+      await context.functions.execute(
+        "middlewareVerificarToken",
+        request.headers,
+        response
+      );
+  
+      const { clienteId } = validate(request);
+  
+      const collectionClientes = context.functions.execute(
+        "getCollectionInstance",
+        "clientes"
+      );
+  
+      const { modifiedCount } = await collectionClientes.updateOne(
+        { _id: BSON.ObjectId(clienteId) },
+        {
+          $set: {
+            deleted: true,
+          },
+        }
+      );
+  
+      if (!modifiedCount)
+        throw new Error("No se pudo eliminar el Cliente seleccionado");
+  
+      context.functions.execute("handlerResponse", response, {
+        deleted: Boolean(modifiedCount),
+        usuarios,
+        ofertasEmpresa,
+      });
+    } catch (err) {
+      context.functions.execute(
+        "handlerResponse",
+        response,
+        null,
+        400,
+        false,
+        err.message
+      );
+    }
+  };
+  
+  const validate = (request) => {
+    const params = { ...request.query };
+  
+    if (!params.clienteId) throw new Error("El id del cliente es requerido");
+  
+    return {
+        clienteId: params.clienteId,
+    };
+  };
+  
