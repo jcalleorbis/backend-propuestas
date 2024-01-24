075533b6480bdff532572161468c4de0b48a9f67
Jorge Luis Calle Morales <jcalle@unifiedsoftware.us>
2024-01-23 22:30:21 -0500

    Complete the upload file

---
+++ functions/registrarFileCliente.js
@@ -0,0 +1,60 @@
+exports = async function (request, response) {
+    try {
+      await context.functions.execute(
+        "middlewareVerificarOrigenClientId",
+        request,
+        response
+      );
+      await context.functions.execute(
+        "middlewareVerificarToken",
+        request.headers,
+        response
+      );
+  
+      const { clienteId, buffer } = validate(request);
+  
+      const collectionClientes = context.functions.execute(
+        "getCollectionInstance",
+        "clientes"
+      );
+  
+      const { modifiedCount } = await collectionClientes.updateOne(
+        { _id: BSON.ObjectId(clienteId) },
+        {
+          $set: {
+            logo: BinData(0, buffer),
+          },
+        }
+      );
+  
+      if (!modifiedCount)
+        throw new Error("No se pudo eliminar el Cliente seleccionado");
+  
+      context.functions.execute("handlerResponse", response, {
+        deleted: Boolean(modifiedCount)
+      });
+    } catch (err) {
+      context.functions.execute(
+        "handlerResponse",
+        response,
+        null,
+        400,
+        false,
+        err.message
+      );
+    }
+  };
+  
+  const validate = (request) => {
+    const params = { ...request.query };
+    const body = JSON.parse(request.body.text())
+  
+    if (!params.clienteId) throw new Error("El id del cliente es requerido");
+    if (!body.buffer) throw new Error("El buffer es requerido");
+  
+    return {
+        clienteId: params.clienteId,
+        buffer: body.buffer
+    };
+  };
+  
